3f86651f1360a93fe13bb6bfc577e3157fa66086
Wenzheng Li <wenzhenl@usc.edu>
2015-12-19 00:28:00 -0800

    update
    Former-commit-id: c0b211adb62468eff6093f5ed5198d9ad92e0f3e

---
+++ Omage/OmageViewController.swift
@@ -0,0 +1,343 @@
+//
+//  ViewController.swift
+//  Omage
+//
+//  Created by Wenzheng Li on 12/18/15.
+//  Copyright © 2015 Wenzheng Li. All rights reserved.
+//
+
+import UIKit
+
+class OmageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, ImageCropViewControllerDelegate {
+
+    @IBOutlet weak var imageContainerView: UIView!
+    
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var foregroundImageView: UIImageView!
+    
+    @IBOutlet weak var tempImageView: UIImageView!
+    
+    @IBOutlet var tapGesture: UITapGestureRecognizer!
+    
+    @IBOutlet var pinchGesture: UIPinchGestureRecognizer!
+    
+    @IBOutlet var rotationGesture: UIRotationGestureRecognizer!
+    
+    @IBOutlet var panGesture: UIPanGestureRecognizer!
+    
+    var photoForBackground = true
+    
+    var eraserDidSelected = false {
+        didSet {
+            tapGesture.enabled = !eraserDidSelected
+            pinchGesture.enabled = !eraserDidSelected
+            rotationGesture.enabled = !eraserDidSelected
+            panGesture.enabled = !eraserDidSelected
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController!.navigationBar.barTintColor = Settings.ColorForHeader
+        self.navigationController!.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController!.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+    }
+    
+    // MARK - background image
+    private var backgroundImage: UIImage? {
+        get { return backgroundImageView?.image }
+        set {
+            backgroundImageView.image = newValue
+            if backgroundImageView.bounds.width > backgroundImage!.size.width && backgroundImageView.bounds.height > backgroundImage!.size.height {
+                print("Center")
+                backgroundImageView.contentMode = .Center
+            } else {
+                print("AspectFit")
+                backgroundImageView.contentMode = .ScaleAspectFit
+            }
+            imageContainerView.bringSubviewToFront(foregroundImageView)
+            imageContainerView.bringSubviewToFront(tempImageView)
+        }
+    }
+    
+    // MARK - foreground image
+    
+    private var foregroundImage: UIImage? {
+        get { return foregroundImageView?.image }
+        set {
+            foregroundImageView.image = newValue
+            foregroundImageView.transform = CGAffineTransformIdentity
+            if foregroundImageView.bounds.width > foregroundImage!.size.width && foregroundImageView.bounds.height > foregroundImage!.size.height {
+                print("Center")
+                foregroundImageView.contentMode = .Center
+            } else {
+                print("AspectFit")
+                foregroundImageView.contentMode = .ScaleAspectFit
+            }
+            self.imageContainerView.bringSubviewToFront(foregroundImageView)
+            self.imageContainerView.bringSubviewToFront(tempImageView)
+        }
+    }
+    
+    @IBAction func pickBackground(sender: UIBarButtonItem) {
+        photoForBackground = true
+        if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+            let picker = UIImagePickerController()
+            picker.sourceType = .PhotoLibrary
+            picker.allowsEditing = false
+            picker.delegate = self
+            presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func pickForeground(sender: UIBarButtonItem) {
+        let foregroundSourceOptions = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.ActionSheet)
+        foregroundSourceOptions.addAction(UIAlertAction(
+            title: "Take photo",
+            style: .Default)
+            { (action:UIAlertAction) -> Void in
+                if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+                    let picker = UIImagePickerController()
+                    picker.sourceType = .Camera
+                    picker.delegate = self
+                    picker.allowsEditing = true
+                    picker.cameraFlashMode = .Off
+                    picker.showsCameraControls = true
+                    
+                    self.presentViewController(picker, animated: true, completion: nil)
+                }
+            })
+        
+        foregroundSourceOptions.addAction(UIAlertAction(
+            title: "Select from photo library",
+            style: .Default)
+            { (action:UIAlertAction) -> Void in
+                self.photoForBackground = false
+                if UIImagePickerController.isSourceTypeAvailable(.PhotoLibrary) {
+                    let picker = UIImagePickerController()
+                    picker.sourceType = .PhotoLibrary
+                    picker.delegate = self
+                    picker.allowsEditing = true
+                    self.presentViewController(picker, animated: true, completion: nil)
+                }
+            })
+        foregroundSourceOptions.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        self.presentViewController(foregroundSourceOptions, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        var image = info[UIImagePickerControllerEditedImage] as? UIImage
+        if image == nil {
+            image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
+        if picker.sourceType == .Camera {
+            foregroundImage = ImageCutoutFilter.cutImageOutWithColor(image, color: Settings.CharPickerPrimaryColor)
+        } else {
+            if photoForBackground {
+                backgroundImage = image
+            } else {
+                foregroundImage = ImageCutoutFilter.cutImageOutWithColor(image, color: Settings.avaiableHandwrittingColors[0])
+            }
+        }
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK - gestures handlers
+    @IBAction func rotateHandWritting(sender: UIRotationGestureRecognizer) {
+        if let imageView = foregroundImageView {
+            switch(sender.state) {
+            case .Ended: fallthrough
+            case .Changed:
+                imageView.transform = CGAffineTransformRotate(imageView.transform, sender.rotation)
+                sender.rotation = 0
+                
+                 print(foregroundImageView.frame, foregroundImageView.bounds)
+            default: break
+            }
+        }
+    }
+    
+    @IBAction func scaleHandwritting(sender: UIPinchGestureRecognizer) {
+        if let imageView = foregroundImageView {
+            switch sender.state {
+            case .Ended: fallthrough
+            case .Changed:
+                imageView.transform = CGAffineTransformScale(imageView.transform, sender.scale, sender.scale)
+                sender.scale = 1
+                
+                 print(foregroundImageView.frame, foregroundImageView.bounds)
+            default: break
+            }
+        }
+    }
+    
+//    @IBAction func changeHandwrittingColor(sender: UITapGestureRecognizer) {
+//        switch sender.state {
+//        case .Ended: fallthrough
+//        case .Changed:
+//            if handwrittingImage != nil {
+//                currentColorIndex = (currentColorIndex + 1) % Settings.avaiableHandwrittingColors.count
+//                handwrittingImageView!.image = SimpleImageProcessor.makeTransparent((opencvImage?.CGImage)!, color: Settings.avaiableHandwrittingColors[currentColorIndex].CGColor)
+//            }
+//        default: break
+//        }
+//    }
+    
+    @IBAction func moveHandwritting(sender: UIPanGestureRecognizer) {
+        if let imageView = foregroundImageView {
+            switch sender.state {
+            case .Ended: fallthrough
+            case .Changed:
+                let translation = sender.translationInView(imageView)
+                imageView.transform = CGAffineTransformTranslate(imageView.transform, translation.x / Settings.GestureScaleForMovingHandwritting, translation.y / Settings.GestureScaleForMovingHandwritting)
+                sender.setTranslation(CGPointZero, inView: imageView)
+                
+                print(foregroundImageView.frame, foregroundImageView.bounds)
+            default: break
+            }
+        }
+    }
+    
+    @IBAction func eraserDidSelected(sender: UIBarButtonItem) {
+        if foregroundImage != nil {
+            eraserDidSelected = !eraserDidSelected
+            foregroundImageView.transform = CGAffineTransformIdentity
+        }
+    }
+    
+    @IBAction func crop(sender: UIBarButtonItem) {
+        if backgroundImage != nil || foregroundImage != nil {
+            let combinedImage = ImageCutoutFilter.convertSnapshotToImage(saveUIViewAsUIImage(imageContainerView))
+            
+            let chopper = ImageCropViewController(image: combinedImage)
+            chopper.blurredBackground = true
+            chopper.delegate = self
+            self.navigationController?.pushViewController(chopper, animated: true)
+        }
+
+    }
+    
+    func ImageCropViewControllerSuccess(controller: UIViewController!, didFinishCroppingImage croppedImage: UIImage!) {
+        backgroundImage = croppedImage
+        foregroundImage = nil
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    func ImageCropViewControllerDidCancel(controller: UIViewController!) {
+        navigationController?.popViewControllerAnimated(true)
+    }
+    
+    // MARK - save an UIView as an UIImage
+    func saveUIViewAsUIImage(view: UIView) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(view.frame.size, false , CGFloat(0.0))
+        view.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return image
+    }
+
+    private var lastPoint = CGPoint.zero
+    private var red: CGFloat = 1.0
+    private var green: CGFloat = 1.0
+    private var blue: CGFloat = 1.0
+    private var brushWidth: CGFloat = 5.0
+    private var opacity: CGFloat = 1.0
+    private var swiped = false
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if eraserDidSelected && foregroundImage != nil {
+            swiped = false
+            if let touch = touches.first {
+                lastPoint = touch.locationInView(tempImageView)
+            }
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if eraserDidSelected && foregroundImage != nil {
+            swiped = true
+            if let touch = touches.first {
+                let currentPoint = touch.locationInView(tempImageView)
+                drawLineFrom(lastPoint, toPoint: currentPoint)
+                
+                lastPoint = currentPoint
+            }
+        }
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if eraserDidSelected && foregroundImage != nil {
+            if !swiped {
+                drawLineFrom(lastPoint, toPoint: lastPoint)
+            }
+            
+            // Merge tempImageView into foregroundImageView
+            UIGraphicsBeginImageContext(foregroundImageView.frame.size)
+            foregroundImageView.image?.drawInRect(getImageRectForImageView(foregroundImageView), blendMode: .Normal, alpha: 1.0)
+            tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: foregroundImageView.frame.size.width, height: foregroundImageView.frame.size.height), blendMode: .Normal, alpha: opacity)
+            foregroundImageView.image = ImageCutoutFilter.cutImageOut(UIGraphicsGetImageFromCurrentImageContext())
+            UIGraphicsEndImageContext()
+            
+            tempImageView.image = nil
+        }
+    }
+    
+    func drawLineFrom(fromPoint: CGPoint, toPoint: CGPoint) {
+        UIGraphicsBeginImageContext(tempImageView.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        
+        tempImageView.image?.drawInRect(CGRect(x: 0, y: 0, width: tempImageView.frame.size.width, height: tempImageView.frame.size.height))
+        print(tempImageView.frame.size.width, tempImageView.frame.size.height)
+        // 2
+        CGContextMoveToPoint(context, fromPoint.x, fromPoint.y)
+        CGContextAddLineToPoint(context, toPoint.x, toPoint.y)
+        
+        // 3
+        CGContextSetLineCap(context, .Round)
+        CGContextSetLineWidth(context, brushWidth)
+        CGContextSetRGBStrokeColor(context, red, green, blue, 1.0)
+        CGContextSetBlendMode(context, .Normal)
+        
+        // 4
+        CGContextStrokePath(context)
+        
+        // 5
+        tempImageView.image = UIGraphicsGetImageFromCurrentImageContext()
+        tempImageView.alpha = opacity
+        UIGraphicsEndImageContext()
+    }
+    
+    func getImageRectForImageView(imageView: UIImageView) -> CGRect {
+        let resVi = imageView.image!.size.width / imageView.image!.size.height
+        let resPl = imageView.bounds.size.width / imageView.bounds.size.height
+        
+        if (resPl > resVi) {
+            
+            let imageSize = CGSizeMake(imageView.image!.size.width * imageView.bounds.size.height / imageView.image!.size.height, imageView.bounds.size.height)
+            return CGRectMake((imageView.bounds.size.width - imageSize.width)/2,
+                (imageView.bounds.size.height - imageSize.height)/2,
+                imageSize.width,
+                imageSize.height)
+        } else {
+            let imageSize = CGSizeMake(imageView.bounds.size.width, imageView.image!.size.height * imageView.bounds.size.width / imageView.image!.size.width)
+            return CGRectMake((imageView.bounds.size.width - imageSize.width)/2,
+                (imageView.bounds.size.height - imageSize.height)/2,
+                imageSize.width,
+                imageSize.height);
+        }
+    }
+}
+
+extension UIImage {
+    var aspectRatio: CGFloat {
+        return size.height != 0 ? size.width / size.height : 0
+    }
+}
+
---
+++ Omage/Settings.swift
@@ -0,0 +1,40 @@
+//
+//  Constants.swift
+//  Alyssa
+//
+//  Created by Wenzheng Li on 10/20/15.
+//  Copyright © 2015 Wenzheng Li. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Settings {
+    // MARK - paramenters for drawing customized buttons
+    static let RatioOfBorderAndWidth = CGFloat(0.1)
+    static let CharPickerPrimaryColor = UIColor(red: 0.39, green: 0.61, blue: 0.96, alpha: 0.9)
+    static let RatioInnerCircleAndHeightForCameraButton = CGFloat(0.7)
+    static let RatioOuterCircleAndHeightForCameraButton = CGFloat(0.9)
+    static let CornerRadiusForPhotoButton = CGFloat(4)
+    static let RatioOuterRectAndHeightForPhotoButton = CGFloat(0.9)
+    static let RatioInnerRectAndHeightForPhotoButton = CGFloat(0.8)
+    static let RatioOuterRectAndHeightForEraserButton = CGFloat(0.8)
+    static let RatioInnerRectAndHeightForEraserButton = CGFloat(0.6)
+    static let RatioInnerCircleAndHeightForColorButton = CGFloat(0.7)
+    static let RatioOuterCircleAndHeightForColorButton = CGFloat(0.9)
+    
+    // MARK - parameters for gestures
+    static let GestureScaleForMovingHandwritting = CGFloat(2.0)
+    
+    // MARK - parameters for color
+    static let avaiableHandwrittingColors =
+    [   UIColor(red: 0, green: 0, blue: 0, alpha: 1.0),
+        UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0),
+        UIColor(red: 0.0078, green: 0.517647, blue: 0.5098039, alpha: 1.0),
+        UIColor(red: 0, green: 0.4, blue: 0.6, alpha: 1.0),
+        UIColor(red: 1.0, green: 0.4, blue: 0, alpha: 1.0)
+    ]
+    
+    // MARK - color for header
+    static let ColorForHeader = UIColor(red: 13.0/255.0, green: 82.0/255.0, blue: 145.0/255.0, alpha: 1.0)
+}
